59beee449c3f8d3931770dc70157175b2bfd276d
whoziee <prachiv24122005@gmail.com>
2024-11-02 16:53:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve index.html for root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
